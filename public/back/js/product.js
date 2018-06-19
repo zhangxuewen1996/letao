2153764c65b436f74ce82d5041866609b22af35b
zhangxuewen1996 <2291742362@qq.com>
2018-06-19 21:16:50 +0800

    商品管理

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -2,6 +2,7 @@ $(function () {
   var pageNum = 1;
   var pageSize = 2;
 
+  //渲染
   function render() {
     $.ajax({
       type: "get",
@@ -43,4 +44,151 @@ $(function () {
     });
   };
   render();
+
+  // 点击添加商品，发送ajax请求生成二级分类的名称
+  $(".lt_main .btn").on("click", function () {
+    $.ajax({
+      type: "get",
+      url: "/category/querySecondCategoryPaging",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      success: function (info) {
+        console.log(info);
+        $(".dropdown-menu").html(template("tpl2", info));
+      }
+    });
+  });
+
+  // 给dropdown-menu下所有的a注册点击事件
+  $(".dropdown-menu").on("click", "a", function () {
+    // 改变dropdown-menu的值
+    $(".dropdown-text").text($(this).text());
+    $("[name='brandId']").val($(this).data("id"));
+    // 手动改变校验结果
+    $("form").data("bootstrapValidator").updateStatus("brandId", "VALID");
+
+  });
+
+  // 进行表单验证
+  $("form").bootstrapValidator({
+    // 1。这里需要注意由于bootstrapValidator默认会不校验type为hidden disabled not(:visible)
+    excluded: [],
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-thumbs-up',
+      invalid: 'glyphicon glyphicon-thumbs-down',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      brandId: {
+        validators: {
+          notEmpty: {
+            message: "请选择一个二级分类",
+          }
+        }
+      },
+      proName: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的名称",
+          }
+        }
+      },
+      proDesc: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的描述",
+          }
+        }
+      },
+      num: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的库存",
+          },
+          regexp: {
+            regexp: /^\d*$/,
+            message: '请输入正确的库存数'
+          }
+        }
+      },
+      size: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的尺码",
+          },
+          regexp: {
+            regexp: /^\d{2}-\d{2}$/,
+            message: '请输入正确的尺码',
+          }
+        }
+      },
+      oldPrice: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的原价",
+          }
+        }
+      },
+      price: {
+        validators: {
+          notEmpty: {
+            message: "请输入商品的价格",
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请输入3张图片",
+          }
+        }
+      }
+    }
+  });
+
+  // 进行上传图片的操作n
+  // 限制只能传三张照片
+  var imgs = [];
+  $("#fileupload").fileupload({
+    type: "json",
+    done: function (e, data) {
+      console.log(data);
+      // 限制只能传三张照片      
+      if (imgs.length >= 3) {
+        return false;
+      }
+      $(".img_box").append('<img src="' + data.result.picAddr + '" width="100" height="100" alt="">');
+      imgs.push(data.result);
+      if (imgs.length == 3) {
+        $("form").data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+      } else {
+        $("form").data("bootstrapValidator").updateStatus("brandLogo", "INVALID");
+      }
+    }
+  });
+
+  // 注册表单校验完成事件
+  $("form").on("success.form.bv", function (e) {
+    e.preventDefault();
+    var $content = $("form").serialize();
+    $content += "&picName1=" + imgs[0].picName + "&picAddr1=" + imgs[0].picAddr;
+    $content += "&picName2=" + imgs[1].picName + "&picAddr2=" + imgs[1].picAddr;
+    $content += "&picName3=" + imgs[2].picName + "&picAddr3=" + imgs[2].picAddr;
+    console.log($content);
+    $.ajax({
+      type: "post",
+      url: "/product/addProduct",
+      data: $content,
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          $("#add-sec").modal("hide");
+          render();
+        }
+      }
+    });
+  });
 });
--- public/back/js/second.js
+++ public/back/js/second.js
@@ -28,4 +28,93 @@ $(function () {
     });
   };
   render();
+
+  // 点击添加分类按钮发送ajax请求
+  $(".lt_main .btn").on("click", function () {
+    $.ajax({
+      type: "get",
+      url: "/category/queryTopCategoryPaging",
+      data: {
+        // 这里的接口文档没有给我们具体的接口，因此使用查询的接口获取数据，将页码和每页的数量定死
+        page: 1,
+        pageSize: 100
+      },
+      success: function (info) {
+        $(".dropdown-menu").html(template("tpl2", info));
+      }
+    });
+  });
+
+  // 给下拉菜单按钮下的a注册点击事件
+  $(".dropdown-menu").on("click", "a", function () {
+    $(".dropdown-text").text($(this).text());
+    $("[name='categoryId']").val($(this).data("id"));
+    $("form").data("bootstrapValidator").updateStatus("categoryId", "VALID");
+  });
+
+  // 上传显示图片的功能
+  $("#uploadPic").fileupload({
+    dataType: "json",
+    done: function (e, data) {
+      console.log(data.result.picAddr);
+      $(".img_box img").attr("src", data.result.picAddr);
+      $("[name='brandLogo']").val(data.result.picAddr);
+      $("form").data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+    }
+  });
+
+  // 表单校验功能
+  $("form").bootstrapValidator({
+    // 1。这里需要注意由于bootstrapValidator默认会不校验type为hidden disabled not(:visible)
+    excluded: [],
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-thumbs-up',
+      invalid: 'glyphicon glyphicon-thumbs-down',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: "请选择一个分类"
+          }
+        }
+      },
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: "请选择一个二级分类"
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请选择一张照片"
+          }
+        }
+      },
+    }
+  });
+
+  // 表单校验成功事件
+  $("form").on("success.form.bv", function () {
+    // 发送ajax请求
+    $.ajax({
+      type: "post",
+      url: "/category/addSecondCategory",
+      data: $("form").serialize(),
+      success: function (info) {
+        if (info.success) {
+          // 影藏模态框
+          $("#add-sec").modal("hide");
+          page = 1;
+          render();
+        }
+      }
+    });
+  });
+
+
 });
